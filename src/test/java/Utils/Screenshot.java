3e05ebadcdb5571512dd31b1d4cca836b6738c7a
Sirenkoa <sandrew@online.ua>
2019-08-02 20:50:19 +0300

    log4j_task1

---
+++ src/test/java/Utils/Screenshot.java
@@ -0,0 +1,36 @@
+package Utils;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Screenshot {
+    private WebDriver driver;
+
+    Screenshot(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    void makeScreenShot(ITestResult result) {
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File src = screenshot.getScreenshotAs(OutputType.FILE);
+        Path currentRelativePath = Paths.get("");
+        try {
+            FileUtils.copyFile(src, new File(
+                    currentRelativePath.toAbsolutePath().toString()
+                            + "\\screenshots\\"
+                            + result.getTestClass().getName().replace(".", "\\\\")
+                            + "\\" + result.getMethod().getConstructorOrMethod().getName() + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
